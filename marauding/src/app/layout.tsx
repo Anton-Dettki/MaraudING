7ba29c3d5246ed49f95dd9f3f897ab3a7c8d807b
Anton Dettki <caspardett@gmail.com>
2025-10-25 16:39:57 +0200

    Add tracking

--- marauding/src/app/layout.tsx
+++ marauding/src/app/layout.tsx
@@ -24,6 +24,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Privacy-friendly analytics by Plausible */}
+        <script async src="https://plausible.io/js/pa-I9Pm59osO84V3TYZAymk5.js"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+              plausible.init()
+            `,
+          }}
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
